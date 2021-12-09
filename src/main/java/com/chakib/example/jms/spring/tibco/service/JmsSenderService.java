11bf15e5b1016a00bab0c5126254c4d10ea190eb
cdaii <chakibdaii1995@gmail.com>
2021-12-09 00:08:18 +0000

    cdaii - Jms service created to send/receive objects to specific queue.

---
+++ src/main/java/com/chakib/example/jms/spring/tibco/service/JmsSenderService.java
@@ -0,0 +1,44 @@
+package com.chakib.example.jms.spring.tibco.service;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class JmsSenderService {
+	
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	@Value("${ems.queue}")
+	private String queue;
+
+	public void send(final String msg) {
+		jmsTemplate.send(queue, session -> {
+			TextMessage message = session.createTextMessage();
+			message.setText(msg);
+			return message;
+		});
+	}
+
+	public String read() throws JMSException {
+		TextMessage message = (TextMessage) jmsTemplate.receive(queue);
+		if(message == null) throw new ResponseStatusException(HttpStatus.NO_CONTENT, "The queue is empty");
+		return message.getText();
+	}
+
+	public void send(final Object msg) {
+		jmsTemplate.convertAndSend(queue, msg);
+	}
+
+	public Object readMessage() {
+		Object message =  jmsTemplate.receiveAndConvert(queue);
+		if(message == null) throw new ResponseStatusException(HttpStatus.NO_CONTENT, "The queue is empty");
+		return message;
+	}
+}
